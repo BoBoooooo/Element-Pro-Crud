a09ae6bc8830f9d8c6e6da996e3aabcd501976c8
BoBoooooo <17746714@qq.com>
2020-03-05 14:59:09 +0800

    feat(index): fix eslint bug

--- packages/index.js
+++ packages/index.js
@@ -8,30 +8,31 @@
 // packages / index.js
 
 // 导入单个组件
-import FormDesigner from './FormDesigner/index'
+import FormDesigner from './FormDesigner/index';
 
 // 以数组的结构保存组件，便于遍历
 const components = [
-    FormDesigner
-]
+  FormDesigner,
+];
 
 // 定义 install 方法
-const install = function (Vue) {
-    if (install.installed) return
-    install.installed = true
-    // 遍历并注册全局组件
-    components.map(component => {
-        Vue.component(component.name, component)
-    })
-}
+const install = (Vue) => {
+  if (install.installed) return;
+  install.installed = true;
+  // 遍历并注册全局组件
+  components.map((component) => {
+    Vue.component(component.name, component);
+    return null;
+  });
+};
 
 if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue)
+  install(window.Vue);
 }
 
 export default {
-    // 导出的对象必须具备一个 install 方法
-    install,
-    // 组件列表
-    ...components
-}
+  // 导出的对象必须具备一个 install 方法
+  install,
+  // 组件列表
+  ...components,
+};
