7d764098d2d3793c5a8c6f7b2933a0be09400b63
BoBoooooo <17746714@qq.com>
2021-04-15 09:33:26 +0800

    ci: add install func

--- src/component/pro-form/index.ts
+++ src/component/pro-form/index.ts
@@ -7,5 +7,16 @@ ProForm.install = function (Vue, options) {
   }
   Vue.component('ProForm', ProForm);
 };
+// 定义 install 方法
+const install: any = (Vue, options = {}) => {
+  if (install.installed) return;
+  install.installed = true;
+  // 遍历并注册全局组件
+  Vue.prototype.$PROCRUD = options;
+  Vue.component('ProForm', ProForm);
+};
 
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
 export default ProForm;
--- src/component/pro-table/index.ts
+++ src/component/pro-table/index.ts
@@ -10,4 +10,17 @@ ProTable.install = function (Vue, options) {
   Vue.component(ProTable.name, ProTable);
 };
 
+// 定义 install 方法
+const install: any = (Vue, options = {}) => {
+  if (install.installed) return;
+  install.installed = true;
+  // 遍历并注册全局组件
+  Vue.prototype.$PROCRUD = options;
+  Vue.component(ProTable.name, ProTable);
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
 export default ProTable;
